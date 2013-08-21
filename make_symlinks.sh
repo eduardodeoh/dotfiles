46bd02822d2f2bb51a1d1fe11034c1d7ae61f3ea
Eduardo de O. Hernandes <eduardodeoh@gmail.com>
2013-08-21 01:05:53 -0300

    Enhancements

--- make_symlinks.sh
+++ make_symlinks.sh
@@ -6,10 +6,23 @@ arr=(fasd gitencrypt)
 
 cd ~; 
 
+#Symlink dotfiles
 for i in `ls dotfiles`
 do
-  if ! [[ ${arr[(r)$i]} == $i ]]
+  #if ! [[ ${arr[(r)$i]} == $i ]]
+  if [ "$i" != "bin" ]
     then
-      ln -nfs dotfiles/$i ~/.${i}
+      echo "ln -nfs dotfiles/$i ~/.${i}"
     fi
 done
+
+
+#Symlink bin
+if ! [ -d ~/bin ]
+then
+  mkdir ~/bin
+  ln -nfs dotfiles/bin ~ 
+else
+  echo "~/bin exists..."
+fi
+
