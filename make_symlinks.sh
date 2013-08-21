ef929137efd8b4b03861e94073f1d2fc2495a929
Eduardo de O. Hernandes <eduardodeoh@gmail.com>
2013-08-21 00:47:50 -0300

    Enhancements

--- make_symlinks.sh
+++ make_symlinks.sh
@@ -1,14 +1,15 @@
 #!/bin/zsh
 
+#Exceptions to .dotfiles at $HOME
 arr=(fasd gitencrypt)
 
+
 cd ~; 
+
 for i in `ls dotfiles`
 do
   if ! [[ ${arr[(r)$i]} == $i ]]
     then
-      echo "ln -nfs dotfiles/$i ~/.${i}"
-    else
-      echo "$arr"
+      ln -nfs dotfiles/$i ~/.${i}
     fi
 done
