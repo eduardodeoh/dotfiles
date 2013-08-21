0f21ba563d5fd0bbcde7ab58f9461aca121e02ad
Eduardo de O. Hernandes <eduardodeoh@gmail.com>
2013-08-21 01:30:13 -0300

    Enhancements

--- make_symlinks.sh
+++ make_symlinks.sh
@@ -9,6 +9,7 @@ cd ~;
 #Symlink dotfiles
 for i in `ls dotfiles`
 do
+  #http://stackoverflow.com/questions/5203665/zsh-check-if-string-is-in-array
   if ! [[ ${arr[(r)$i]} == $i ]]
     then
       echo "ln -nfs dotfiles/$i ~/.${i}"
@@ -20,8 +21,12 @@ done
 if ! [ -d ~/bin ]
 then
   mkdir ~/bin
-  ln -nfs ~/dotfiles/bin_sources ~ 
 else
   echo "~/bin exists..."
 fi
+
+#arrbin=(fasd git-encrypt)
+#for i in $arrbin
+#do
+#  ln -nfs ~/dotfiles/bin ~/bin/
 
