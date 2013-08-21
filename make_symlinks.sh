a566abafbb0346246a9a6e480ce5ca98f539776c
Eduardo de O. Hernandes <eduardodeoh@gmail.com>
2013-08-21 00:43:08 -0300

    Enhancements...

--- make_symlinks.sh
+++ make_symlinks.sh
@@ -1,2 +1,14 @@
-#!/usr/bin/env sh
-cd ~; for i in `ls dotfiles`; do ln -nfs dotfiles/$i ~/.${i}; done
+#!/bin/zsh
+
+arr=(fasd gitencrypt)
+
+cd ~; 
+for i in `ls dotfiles`
+do
+  if ! [[ ${arr[(r)$i]} == $i ]]
+    then
+      echo "ln -nfs dotfiles/$i ~/.${i}"
+    else
+      echo "$arr"
+    fi
+done
