f665cfd165754ddba650d438caf17909560baf82
Eduardo de O. Hernandes <eduardodeoh@gmail.com>
2014-04-19 16:32:11 -0300

    Change to install oh-my-zhs powerline theme

--- zsh/oh-my-zsh/bootstrap-oh-my-zsh.sh
+++ zsh/oh-my-zsh/bootstrap-oh-my-zsh.sh
@@ -1,3 +1,9 @@
+#Install oh-my-zsh
 git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
+
+#Install powerline theme
+$HOME/dotfiles/zsh/oh-my-zsh/custom/oh-my-zsh-powerline-theme/install_in_omz.sh
+
+#Make symlink to zshrc
 ln -sf $HOME/dotfiles/zsh/oh-my-zsh/zshrc $HOME/.zshrc
 
